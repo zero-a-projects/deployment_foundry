bf3a55c1d5dc20f4b550e03ba9f6cb832a953cd3
xiaodong.wang <kk98.young@gmail.com>
2024-07-25 17:44:59 +0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: [""], //Your private key starting with "0x" 
+      //Make sure you have enough funds in this wallet to deploy the smart contract
+    },
+  },
+};
